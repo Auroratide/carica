cfa908a2cc8741d3afedbd29d42feb32af02ea0b
Timothy Foster <tf.auroratide@gmail.com>
2021-08-25 14:03:32 -0500

    More official layer attribute

--- src/internal/svg-processor/divide-into-layers.spec.ts
+++ src/internal/svg-processor/divide-into-layers.spec.ts
@@ -16,32 +16,44 @@ describe('divideIntoLayers', () => {
         expect(result).to.be.empty
     })
 
-    it('svg with defined layers', async () => {
+    it('svg with named layers', async () => {
         const svg = await fixture<SVGSVGElement>(html`
             <svg viewBox="0 0 1 2">
-                <g data-layer="2"></g>
-                <g data-layer="1"></g>
-                <path data-layer="3" d="" />
+                <g carica:layer="something"></g>
+                <g carica:layer="else"></g>
+                <path carica:layer="something" d="" />
             </svg>
         `)
 
         const result = divideIntoLayers([svg])
 
-        
         expect(result).to.have.length(3)
         expect(result[0].getAttribute('viewBox')).to.equal('0 0 1 2')
-        expect(result[0].style.zIndex).to.equal('2')
+        expect(result[0].style.zIndex).to.equal('var(--something_layer)')
         expect(result[1].getAttribute('viewBox')).to.equal('0 0 1 2')
-        expect(result[1].style.zIndex).to.equal('1')
+        expect(result[1].style.zIndex).to.equal('var(--else_layer)')
         expect(result[2].getAttribute('viewBox')).to.equal('0 0 1 2')
-        expect(result[2].style.zIndex).to.equal('3')
+        expect(result[2].style.zIndex).to.equal('var(--something_layer)')
+    })
+
+    it('svg with anonymous layers', async () => {
+        const svg = await fixture<SVGSVGElement>(html`
+            <svg viewBox="0 0 1 2">
+                <g carica:layer="2"></g>
+            </svg>
+        `)
+
+        const result = divideIntoLayers([svg])
+
+        expect(result).to.have.length(1)
+        expect(result[0].style.zIndex).to.equal('2')
     })
 
     it('svg with undefined layers', async () => {
         const svg = await fixture<SVGSVGElement>(html`
             <svg viewBox="0 0 1 2">
                 <g></g>
-                <g data-layer="3"></g>
+                <g carica:layer="3"></g>
             </svg>
         `)
 
@@ -55,7 +67,7 @@ describe('divideIntoLayers', () => {
         const svg = await fixture<SVGSVGElement>(html`
             <svg viewBox="0 0 1 2">
                 <g>
-                    <g data-layer="2"></g>
+                    <g carica:layer="2"></g>
                 </g>
             </svg>
         `)
--- src/internal/svg-processor/divide-into-layers.ts
+++ src/internal/svg-processor/divide-into-layers.ts
@@ -3,11 +3,22 @@ export const divideIntoLayers: (elements: SVGSVGElement[]) => SVGSVGElement[] = 
         return Array.from(svg.children).map((layer) => {
             const svgLayer = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
             svgLayer.setAttribute('viewBox', svg.getAttribute('viewBox') ?? '')
-            svgLayer.style.zIndex = layer.getAttribute('data-layer') ?? ''
+            svgLayer.style.zIndex = getLayerValue(layer)
             
             svgLayer.appendChild(layer)
 
             return svgLayer
         })
     })
+}
+
+const getLayerValue: (layer: Element) => string = (layer) => {
+    const value = layer.getAttribute('carica:layer') ?? ''
+
+    // empty or a number
+    if (/^\s*\d*\s*$/.test(value)) {
+        return value
+    } else {
+        return `var(--${value}_layer)`
+    }
 }
