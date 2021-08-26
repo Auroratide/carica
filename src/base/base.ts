75feb81aa57a90ce321fbb06597c9c98aaa87aa1
Timothy Foster <tf.auroratide@gmail.com>
2021-08-26 10:51:29 -0500

    Establish layers

--- src/base/base.ts
+++ src/base/base.ts
@@ -6,6 +6,24 @@ const css = `
     :host {
         --carica-color_undefined: #ff00ff;
 
+        --back_layer: 0;
+        --afore-back_layer: 4;
+
+        --behind-body_layer: 8;
+        --body_layer: 12;
+        --afore-body_layer: 16;
+
+        --behind-head_layer: 20;
+        --head_layer: 24;
+        --afore-head_layer: 28;
+
+        --behind-face_layer: 32;
+        --face_layer: 36;
+        --afore-face_layer: 40;
+
+        --behind-front_layer: 44;
+        --front_layer: 48;
+
         display: block;
         position: relative;
     }
